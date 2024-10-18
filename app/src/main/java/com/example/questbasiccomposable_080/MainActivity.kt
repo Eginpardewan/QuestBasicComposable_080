c78e472bcc5ea00401649558a2f32048ec259368
Egin Pardewan <eginpardewan@gmail.com>
2024-10-18 22:11:51 +0700

    menambahkan preview untuk BasicComposse function dengan aligment.center

--- app/src/main/java/com/example/questbasiccomposable_080/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable_080/MainActivity.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.questbasiccomposable_080.ui.theme.QuestBasicComposable_080Theme
@@ -29,15 +32,18 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
+@Composable
+fun BasicComposse(modifier: Modifier = Modifier) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier.fillMaxSize()
+    ){
+
+    }
+
+        @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     QuestBasicComposable_080Theme {
