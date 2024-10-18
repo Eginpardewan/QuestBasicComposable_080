5c48caf7f9f9f98578b3cfc0344ac464cc5fe780
Egin Pardewan <eginpardewan@gmail.com>
2024-10-18 22:29:12 +0700

    Menambahkan komponen logoumy.jpeg untuk menampilkan logo

--- app/src/main/java/com/example/questbasiccomposable_080/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable_080/MainActivity.kt
@@ -4,16 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -63,6 +66,10 @@ fun BasicComposse(modifier: Modifier = Modifier) {
                 fontWeight = FontWeight.Normal,
                 color = Color.Blue
             ),
+        )
+        Image(
+            painter = painterResource(id = R.drawable.logoumy), contentDescription = null,
+            modifier = Modifier.size(150.dp)
         )
     }
 
